d0aea344465f5d174baa807589ab76639959ee3d
AndreaSzoboszlai <szobandi@gmail.com>
2019-02-10 19:20:08 +0100

    initial

--- src/main/java/com/codecool/plaza/api/ClothingProduct.java
+++ src/main/java/com/codecool/plaza/api/ClothingProduct.java
@@ -4,5 +4,24 @@ import java.util.Date;
 
 
 public class ClothingProduct extends Product {
-    
+    private String material;
+    private String type;
+
+    public ClothingProduct(long barcode, String name, String manufacturer, String material, String type) {
+        super(barcode, name, manufacturer);
+        this.material = material;
+        this.type = type;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String toString() {
+        return getBarCode() + ", " + getName() + ", " + getManufacturer() + ", " + material + ", " + type;
+    }
 }
